d1727a7ffa7564b7c1b0f05f7e1a364f71a54619
Thiago Casiano <162317115+thiago280922@users.noreply.github.com>
2024-05-17 17:38:53 -0300

    Add files via upload

---
+++ desafio2ThiagoC.js
@@ -0,0 +1,14 @@
+alert("bienvenido amiguillo")
+
+let usuario = prompt("ingresar su nombre de usuario")
+
+let nombreProducto = prompt("ingrese el nombre del producto")
+
+let precioUnitario = prompt("ingrese el valor del producto")
+
+let cantDeseada = prompt("ingresar la cantidad de productos que desee")
+
+costoTotal = precioUnitario * cantDeseada
+
+alert(costoTotal)
+
