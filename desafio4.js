c24b7b1f3662ea565c066945e3dd1239568045e6
Thiago Casiano <162317115+thiago280922@users.noreply.github.com>
2024-05-17 20:34:35 -0300

    Add files via upload

---
+++ desafio4.js
@@ -0,0 +1,9 @@
+function sumarProductos (precio,cantidadDeseada){
+
+    let precio = prompt("ingresar el precio del producto")
+    let cantidadDeseada = prompt("Ingresar la cantidad deseada")
+    
+    let totalGastado = precio * cantidadDeseada
+    
+    console.log(totalGastado)
+    }
